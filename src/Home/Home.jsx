5e988680cbf3cd5767a7303e6e7c020487679076
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-21 17:04:50 -0500

    Avance1

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,34 +1,23 @@
+
 import "./Home.css"
 
-import { inventario } from "../utils/medicamentos"
+import basedatos from"../utils/basedatos.json"
 
 export function Home(){
 
-    //A LOS ARREGLOS LOS PUEDO MANIPULAR
-    //PARA CONTROLAR QUE INFORMACION VOY A PRESENTAR
-    
-    //1. FUNCION DE FILTRADO DE DATOS
-    //CONDICION DE FILTRADO (PREGUNTA (SI/NO))
-    inventario
+    console.log(basedatos[0])
 
-
-//zona de manipulacion de datos
-let nombrePaciente="Juan Jose Gallego"
-let sedePaciente="Suramericana"
-
-console.log(inventario)
-
-
+  
     return(//zona de renderizado
         <>
             <section className="container mt-5">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <h3 className="fuente">Servicios a un clic</h3>
-                        <h2 className="text-muted">HOLA <span className="fuente">{nom}</span></h2>
+                        <h2 className="text-muted">HOLA <span className="fuente"></span></h2>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque delectus nesciunt tenetur error nostrum enim quam corrupti praesentium, facere quasi mollitia accusamus, modi rem reprehenderit ad incidunt excepturi tempore, quia neque officia doloribus. Facilis excepturi qui minus consectetur illum numquam, magni soluta, dolor ducimus culpa veritatis vel dignissimos, consequuntur aliquam?</p>
                     </div>
-                    <div className="col-12 col-md-6">
+                        <div className="col-12 col-md-6">
                         <select class="form-select" aria-label="Default select example">
                             <option selected>Tu Grupo Familiar</option>
                             <option value="1">One</option>
@@ -43,20 +32,76 @@ console.log(inventario)
                             <div className="col-12 col-md-10 mt-5">
                                 <br />
                                <p>Juan Jose Graciano</p> 
+                               <hr />
                             </div>
                         </div>
                         <div className="row">
+                        <div className="col-12 col-md-2 mt-5">
+                            <i class="bi bi-clipboard2-pulse fs-1 fuente"></i>
+                            </div>
+                            <div className="col-12 col-md-10 mt-5">
+                                <br />
+                               <p>Medico Familia</p> 
+                               <hr />
+                            </div>
 
                         </div>
                         <div className="row">
+                        <div className="col-12 col-md-2 mt-5">
+                        <i class="bi bi-piggy-bank fs-1 fuente"></i>
+                            </div>
+                            <div className="col-12 col-md-10 mt-5">
+                                <br />
+                               <p>Grupo de ingreso</p> 
+                               <hr />
+                            </div>
 
                         </div>
                         <div className="row">
+                        <div className="col-12 col-md-2 mt-5">
+                        <i class="bi bi-hospital fs-1 fuente"></i>
+                            </div>
+                            <div className="col-12 col-md-10 mt-5">
+                                <br />
+                               <p>IPS</p> 
+                               <hr />
+                            </div>
 
                         </div>
 
                     </div>
                 </div>
+            </section>
+
+            <hr />
+            <section className="container my-5">
+                <div className="row">
+                    <div className="col-12 col-md-5">
+                        <h5> PACIENTE {basedatos[0].nombreUsuario} estas son tus proximas citas:
+                             </h5>
+
+                    </div>
+                </div>
+            </section>
+            <section className="container">
+                <div className="row row-cols-1 row-cols-md-3">
+                    {
+                        basedatos[0].citasMedicas.map(function(cita){
+                            return(
+                                <div className="col">
+                                    <div className="card h-100 shadow px-2">
+                                        <h3>{cita.especialidad}</h3>
+                                        <h4>Fecha: {cita.fecha} </h4>
+                                        <h4>{cita.direccion}</h4>
+
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+
             </section>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import { Home } from './Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <Home></Home>
+    
   </React.StrictMode>,
 )
